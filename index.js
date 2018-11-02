7f13132ef8fd68906f85af28e25c410f6d8a85eb
Sergio Neves <serneves@hotmail.com>
2018-11-02 23:24:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function happyHolidaysTo(name) {
   return `Happy Holidays, ${name}!`
 }
 
-function happyHolidayTo(holiday; name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}! `
 }
 
-function holidayCountdown(days; holiday) {
+function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`
 }
